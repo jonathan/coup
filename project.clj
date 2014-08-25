6b0f8bf40962944051208b1ccc4f7e26e56f2342
Jonathan Hicks <jonathan.r.hicks@gmail.com>
2014-08-25 10:40:36 -0400

    Adding core.async to try out making the algorithm async

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://github.com/jonathan/coup"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]]
   :main ^:skip-aot coup.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
--- src/ga/core.clj
+++ src/ga/core.clj
@@ -1,4 +1,7 @@
-(ns ga.core)
+(ns ga.core
+  (:refer-clojure :exclude [map reduce into partition partition-by take merge])
+  (require [clojure.core.async :as async :refer :all]
+           [clojure.pprint :refer [pprint]]))
 
 ;; Variables for evolving a solution
 (def population-size 300)
@@ -19,9 +22,7 @@
   "Create a hash-map that represents an individual. The keys are
   :chromosome and :fitness."
   []
-  (let [chromosome (repeatedly (count ideal) #(rand-nth ideal))
-        fit (fitness chromosome)]
-    {:chromosome chromosome}))
+  {:chromosome (repeatedly (count ideal) #(rand-nth ideal))})
 
 (defn gen-population
   "Creates a sequence of individuals based on the passed in size."
@@ -31,61 +32,63 @@
 (defn gen-tournaments
   "Creates a sequence of sequences where each sub-seq is a list of
   individuals. These individuals are 'competing' based on their fitness."
-  [population]
-  (concat
-    (partition tourney-size population)
-    (repeatedly (/ population-size winner-size) (fn [] (repeatedly tourney-size (fn [] (rand-nth population)))))
-    (repeatedly (/ population-size winner-size) (fn [] (repeatedly tourney-size (fn [] (rand-nth population)))))))
+  [population tourney]
+    (go (>! tourney (partition tourney-size population)))
+    (go (>! tourney (repeatedly (/ population-size winner-size) (fn [] (repeatedly tourney-size (fn [] (rand-nth population)))))))
+    (go (>! tourney (repeatedly (/ population-size winner-size) (fn [] (repeatedly tourney-size (fn [] (rand-nth population))))))))
 
-(defn top-winners
-  "Returns the a number of winners from the given tournament seq."
-  [tournament]
-  (let [tourney-results (map #(assoc % :fitness (fitness (:chromosome %))) tournament)]
-    (take winner-size (sort-by :fitness > tourney-results))))
-
-(defn crossover
-  "Takes two individuals and determines if crossover should be performed
-  on them based off of the crossover-pct. Returnes new individals if
-  crossover should be performed or returns the original individuals otherwise"
-  [{ind1 :chromosome} {ind2 :chromosome}]
-  (if (> crossover-pct (rand-int 100))
-    (let [cross-point (rand-int (count ideal))]
-      (list {:chromosome (concat (take cross-point ind1) (drop cross-point ind2))}
-            {:chromosome (concat (take cross-point ind2) (drop cross-point ind1))}))
-    (list {:chromosome ind1}
-          {:chromosome ind2})))
-
-(defn mutate
-  "Takes a population sequence and determines if the any of the individuals
-  should be mutated. Returnes a new population with any mutated individuals."
-  [population]
-  (map (fn [ind]
-            (if (> mutate-pct (rand-int 100))
-            (let [chrom (seq (assoc (vec (:chromosome ind)) (rand-int (count ideal)) (rand-nth ideal)))
-                  fitness (fitness chrom)]
-              {:chromosome chrom
-               :fitness fitness})
-            ind)) population))
-
-(defn found-max
-  "Checks if any individual in the population matches the ideal."
-  [population]
-  (some #(= (count ideal) (:fitness %)) population))
-
-(defn stopping-criteria
-  "Checks if evolution for a given population and generation should continue."
-  [population gen-num]
-  (and (<= gen-num 200) (not (found-max population))))
-
-(defn evolve
-  "Evolves a solution closest to the ideal chromosome."
-  []
-  (loop [population (gen-population population-size)
-         gen-num 0]
-    (if (stopping-criteria population gen-num)
-      (let [tourneys (gen-tournaments population)
-            winners (map top-winners tourneys)
-            new-pop (flatten (map #(crossover (first %) (second %)) winners))]
-        (recur (mutate new-pop) (inc gen-num)))
-      (first (filter #(= (count ideal) (:fitness %)) population)))))
+;(defn top-winners
+;  "Returns the a number of winners from the given tournament seq."
+;  [tournament winners]
+;  (let [tourney-results (map #(assoc % :fitness (fitness (:chromosome %))) (go (<! tournament)))]
+;    (go (>! winners (take winner-size (sort-by :fitness > tourney-results))))))
+;
+;(defn crossover
+;  "Takes two individuals and determines if crossover should be performed
+;  on them based off of the crossover-pct. Returnes new individals if
+;  crossover should be performed or returns the original individuals otherwise"
+;  [{ind1 :chromosome} {ind2 :chromosome}]
+;  (if (> crossover-pct (rand-int 100))
+;    (let [cross-point (rand-int (count ideal))]
+;      (list {:chromosome (concat (take cross-point ind1) (drop cross-point ind2))}
+;            {:chromosome (concat (take cross-point ind2) (drop cross-point ind1))}))
+;    (list {:chromosome ind1}
+;          {:chromosome ind2})))
+;
+;(defn mutate
+;  "Takes a population sequence and determines if the any of the individuals
+;  should be mutated. Returnes a new population with any mutated individuals."
+;  [population]
+;  (map (fn [ind]
+;            (if (> mutate-pct (rand-int 100))
+;            (let [chrom (seq (assoc (vec (:chromosome ind)) (rand-int (count ideal)) (rand-nth ideal)))
+;                  fitness (fitness chrom)]
+;              {:chromosome chrom
+;               :fitness fitness})
+;            ind)) population))
+;
+;(defn found-max
+;  "Checks if any individual in the population matches the ideal."
+;  [population]
+;  (some #(= (count ideal) (:fitness %)) population))
+;
+;(defn stopping-criteria
+;  "Checks if evolution for a given population and generation should continue."
+;  [population gen-num]
+;  (and (<= gen-num 200) (not (found-max population))))
+;
+;(defn evolve
+;  "Evolves a solution closest to the ideal chromosome."
+;  []
+;  (loop [population (gen-population population-size)
+;         gen-num 0]
+;    (if (stopping-criteria population gen-num)
+;      (let [tourney (chan)
+;            winners (chan)
+;            new-pop (chan)]
+;            (gen-tournaments population tourney)
+;            (map top-winners tourney winners)
+;            (go (<! new-pop (flatten (map #(crossover (first %) (second %)) (go (<! winners))))))
+;        (recur (mutate new-pop) (inc gen-num)))
+;      (first (filter #(= (count ideal) (:fitness %)) population)))))
 
